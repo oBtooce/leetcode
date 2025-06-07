b55df6d4f779afe63f86b16d412cd7caf9ee7f8d
alexwaddell <alex.b.waddell@gmail.com>
2025-06-07 16:46:58 -0400

    Solution for Leetcode problem #0001: Two Sum

---
+++ 0001-TwoSum/0001-TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] nums = { 3, 2, 4 };
+int target = 6;
+
+TwoSum(nums, target);
+
+int[] TwoSum(int[] nums, int target)
+{
+    int[] indices = new int[2];
+    int diff;
+    int lastOccurrence;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        // Need to account for positive and negative values
+        diff = target - nums[i];
+        lastOccurrence = Array.LastIndexOf(nums, diff);
+
+        if ((diff * 2 == target && lastOccurrence != i) || (diff * 2 != target && lastOccurrence > -1))
+        {
+            indices = [i, lastOccurrence];
+            break;
+        }
+    }
+
+    return indices;
+}
